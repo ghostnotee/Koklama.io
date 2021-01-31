66955ad1b901895516d5cf59f9a1260017580368
Selçuk Bilgen <selcukbilgen@hotmail.com>
2021-02-01 00:59:09 +0300

    Business Layer created

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            // Business Codes
+            // Authorization ?
+            // Authentication ?
+
+            return _productDal.GetAll();
+        }
+    }
+}
